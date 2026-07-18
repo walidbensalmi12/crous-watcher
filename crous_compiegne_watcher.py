aefb2c3669beff3a81265f89addf3429903d582a
walidbensalmi12 <bw.reports1@gmail.com>
2026-07-18 10:50:53 +0100

    crous_compiegne_watcher.py

--- crous_compiegne_watcher.py
+++ crous_compiegne_watcher.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*- 
 """
 crous_compiegne_watcher.py  (cloud / GitHub Actions version)
 ==============================================================
